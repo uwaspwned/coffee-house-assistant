4a0938041099081c8de107bd6aa0f68b30e0311c
pavel dotwired <pavel.wired@gmail.com>
2026-06-07 11:55:56 +0600

    Added menu and reset command

--- bot.py
+++ bot.py
@@ -7,12 +7,14 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 from config import Config
+from prompts import MENU
 
 logging.basicConfig(level=logging.INFO)
 
 API_TOKEN=Config.TELEGRAM_BOT_TOKEN
 
-bot = Bot(token=API_TOKEN) # type: ignore
+from proxy import session # delete this before commit
+bot = Bot(token=API_TOKEN, session=session) # type: ignore
 
 dp = Dispatcher() # type: ignore
 
@@ -22,10 +24,20 @@ async def cmd_start(message: Message):
     await message.answer("Hi! I'm a coffee shop bot. How can I help you?")
 
 
+@dp.message(Command("menu"))
+async def show_menu(message: Message):
+    await message.answer(f"Here is our menu: \n{MENU}")
+
+
+dp.message(Command("reset"))
+async def reset(message: Message):
+    await message.answer("The history was resets")
+
+
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+        asyncio.run(main()) # type: ignore
